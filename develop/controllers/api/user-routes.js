a18732b584a0ac5932ba9795afd93477c6a7e971
Fabitheguy003 <fabienmukungu@gmail.com>
2023-03-07 21:47:54 -0500

    added user-router.js

--- develop/controllers/api/user-routes.js
+++ develop/controllers/api/user-routes.js
@@ -7,9 +7,7 @@ const router = Router();
 router.get('/', async (req, res) => {
   try {
     const userData = await User.findAll({
-      attributes: {
-        exclude: ['password']
-      }
+      attributes: { exclude: ['password'] }
     });
     res.json(userData);
   } catch (err) {
@@ -22,32 +20,15 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const userData = await User.findOne({
-      attributes: {
-        exclude: ['password']
-      },
-      where: {
-        id: req.params.id
-      },
+      attributes: { exclude: ['password'] },
+      where: { id: req.params.id },
       include: [
-        {
-          model: Post,
-          attributes: ['id', 'title', 'content', 'created_at']
-        },
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'created_at'],
-          include: {
-            model: Post,
-            attributes: ['title']
-          }
-        }
+        { model: Post, attributes: ['id', 'title', 'content', 'created_at'] },
+        { model: Comment, attributes: ['id', 'comment_text', 'created_at'], include: { model: Post, attributes: ['title'] } }
       ]
     });
-    if (!userData) {
-      res.status(404).json({ message: 'No user found with this id' });
-      return;
-    }
-    res.json(userData);
+    if (!userData) res.status(404).json({ message: 'No user found with this id' });
+    else res.json(userData);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -57,10 +38,7 @@ router.get('/:id', async (req, res) => {
 // Create a user
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create({
-      username: req.body.username,
-      password: req.body.password
-    });
+    const userData = await User.create({ username: req.body.username, password: req.body.password });
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.username = userData.username;
@@ -76,33 +54,20 @@ router.post('/', async (req, res) => {
 // User login
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({
-      where: {
-        username: req.body.username
+    const userData = await User.findOne({ where: { username: req.body.username } });
+    if (!userData) res.status(400).json({ message: 'No user with that username!' });
+    else {
+      const validPassword = userData.checkPassword(req.body.password);
+      if (!validPassword) res.status(400).json({ message: 'Incorrect password!' });
+      else {
+        req.session.save(() => {
+          req.session.user_id = userData.id;
+          req.session.username = userData.username;
+          req.session.loggedIn = true;
+          res.json({ user: userData, message: 'You are now logged in!' });
+        });
       }
-    });
-    if (!userData) {
-      res.status(400).json({ message: 'No user with that username!' });
-      return;
     }
-
-    const validPassword = userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.username = userData.username;
-      req.session.loggedIn = true;
-
-      res.json({
-        user: userData,
-        message: 'You are now logged in!'
-      });
-    });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -111,13 +76,8 @@ router.post('/login', async (req, res) => {
 
 // User logout
 router.post('/logout', (req, res) => {
-  if (req.session.loggedIn) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
+  if (req.session.loggedIn) req.session.destroy(() => res.status(204).end());
+  else res.status(404).end();
 });
 
 export default router;
